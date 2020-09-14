eb13946a00cd62d90d22401dc5cff364503af80a
Maliha Tanweer <malihatanweer@gmail.com>
2020-09-14 17:36:05 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <BrowserRouter> 
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/" component={ShopPage} />
