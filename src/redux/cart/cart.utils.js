b13e002a2d97b6604d29b180aa58ccc060ee96a7
Maliha Tanweer <malihatanweer@gmail.com>
2020-10-02 20:04:06 -0400

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,10 +2,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(cartItem => 
     cartItem.id === cartItemToAdd.id);
 
+  // if its alreay in the cart
   if (existingCartItem) {
     return cartItems.map(cartItem => 
       cartItem.id === cartItemToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1} 
       : cartItem )
   }
+  // if not in cart
   return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }  
